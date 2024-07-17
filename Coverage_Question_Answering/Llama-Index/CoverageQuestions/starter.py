c28e899dd526d9ef75c24077ce57d578df1b2c81
Samveg13 <samveg.shah@coalitioninc.com>
2024-07-17 14:56:16 -0500

    Delete Coverage_Question_Answering directory

--- Coverage_Question_Answering/Llama-Index/CoverageQuestions/starter.py
+++
@@ -1,7 +0,0 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
-PERSIST_DIR = "./storage"
-
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
-index.storage_context.persist(persist_dir=PERSIST_DIR)
